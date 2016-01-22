8aa5878c697c07c17973a68d9637bd02dd78e1c8
Stely66 <mad_stely@yahoo.com>
2016-01-22 11:48:02 +0200

    Update login.js
    changed ip address to login to alfresco

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
-    browser.driver.get('http://127.0.0.1:8080/share/page/');
+    browser.driver.get('http://192.168.96.20:8080/share/page/');
     browser.driver.findElement(by.name('username')).sendKeys('admin');
 	browser.driver.findElement(by.name('password')).sendKeys('admin');
 	browser.driver.findElement(by.id('page_x002e_components_x002e_slingshot-login_x0023_default-submit-button')).click();
